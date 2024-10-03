2b3101eb0bff59c74331e613d7b285095b814e6f
hector.romang <hector.romang@durango.gob.mx>
2024-10-03 15:56:23 -0600

    InputSignals y HostBinding

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,14 +1,25 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
   standalone: true,
   imports: [],
   templateUrl: './calculator-button.component.html',
+  styleUrl: './calculator-button.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
   //Con esta propiedad agregamos los estilos a nuestro componente y cuando lo mandemos a llamr siempre lo respetara
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400'
   }
 })
-export class CalculatorButtonComponent { }
+export class CalculatorButtonComponent {
+  public isCommand = input(false, {
+    transform: ( value: boolean | string ) =>
+      typeof value === 'string' ? value === '' : value
+  });
+
+  //Este decorador tiene el acceso y todas las propiedades del host en el decorador @Component
+  @HostBinding('class.is-command') get commandStyle() {
+    return this.isCommand();
+  }
+ }
