1b0323547b29ec791a69641cf794ae1a481b1cdc
hector.romang <hector.romang@durango.gob.mx>
2024-10-03 15:07:17 -0600

    Separacion de componentes

--- src/app/calculator/views/calculator-view/calculator-view.component.ts
+++ src/app/calculator/views/calculator-view/calculator-view.component.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalculatorComponent } from "../../components/calculator/calculator.component";
 
 @Component({
   selector: 'calculator-view',
   standalone: true,
-  imports: [],
+  imports: [
+    CalculatorViewComponent,
+    CalculatorComponent
+],
   templateUrl: './calculator-view.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
