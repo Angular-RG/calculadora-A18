96d68553ccaee0cb4514e45888caaa38b58eca77
HectorARG <hectorr9577@gmail.com>
2024-10-03 20:13:12 -0600

    ViewEncapsulation

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -10,16 +10,29 @@ import { ChangeDetectionStrategy, Component, HostBinding, input } from '@angular
   //Con esta propiedad agregamos los estilos a nuestro componente y cuando lo mandemos a llamr siempre lo respetara
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400'
-  }
+  },
+  // Esta puede ser otra opcion
+  // encapsulation: ViewEncapsulation.None
 })
 export class CalculatorButtonComponent {
+
   public isCommand = input(false, {
     transform: ( value: boolean | string ) =>
       typeof value === 'string' ? value === '' : value
   });
 
+  public isDoubleSize = input(false, {
+    transform: ( value: boolean | string ) =>
+      typeof value === 'string' ? value === '' : value
+  });
+
   //Este decorador tiene el acceso y todas las propiedades del host en el decorador @Component
-  @HostBinding('class.is-command') get commandStyle() {
-    return this.isCommand();
+  // @HostBinding('class.is-command') get commandStyle() {
+  //   return this.isCommand();
+  // }
+
+  //  Este decorador tiene el acceso y todas las propiedades del host en el decorador @Component
+  @HostBinding('class.w-2/4') get commandStyle() {
+    return this.isDoubleSize();
   }
  }
--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -9,5 +9,11 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
   ],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  // Esta es otra opcion posible
+  // styles: `
+  //   .is-command {
+  //   @apply bg-indigo-100 bg-opacity-10 hover:bg-opacity-20 text-2xl;
+  // }
+  // `
 })
 export class CalculatorComponent { }
